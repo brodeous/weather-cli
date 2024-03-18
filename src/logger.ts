352bfd43c7d7d1b06e4e07d29647c8085335c002
NMeekGit <noahmeeker@gmail.com>
2024-03-17 18:48:13 -0700

    updated logging > logs now print to file

--- src/logger.ts
+++ src/logger.ts
@@ -1,19 +1,40 @@
 import chalk from 'chalk';
+import fs from 'fs';
+import config from './config.js';
+
+const currentTime = new Date();
+const currentHour = currentTime.getHours() < 10 ? `${0}${currentTime.getHours()}` : `${currentTime.getHours()}`;
+const currentMinute = currentTime.getMinutes() < 10 ? `${0}${currentTime.getMinutes()}` : `${currentTime.getMinutes()}`;
+const currentSecond = currentTime.getSeconds() < 10 ? `${0}${currentTime.getSeconds()}` : `${currentTime.getSeconds()}`;
+const time = chalk.cyan(`${currentHour}:${currentMinute}:${currentSecond}`);
 
 const info = (msg: string) => {
-    console.log(chalk.white(`[i] ${msg}`));
+    const text = chalk.white(`[i] ${msg}`);
+    printLog(text);
 }
 
 const ok = (msg: string) => {
-    console.log(chalk.green(`[+] ${msg}`));
+    const text = chalk.green(`[+] ${msg}`);
+    printLog(text);
 }
 
 const warn = (msg: string) => {
-    console.log(chalk.yellow(`[w] ${msg}`));
+    const text = chalk.green(`[w] ${msg}`);
+    printLog(text);
 }
 
 const error = (msg: string) => {
-    console.log(chalk.red(`[!]`), `${msg}`);
+    const text = chalk.red(`[!]`) + ` ${msg}`;
+    printLog(text);
+}
+
+const printLog = (log: string) => {
+    fs.appendFile("logs", `[${time}] ${log}\n`, (error) => {
+        if (error) throw error;
+    });
+
+    if (config.isDev)
+        console.log(log);
 }
 
 export {
