b8c1eecf9bfe4c03f760cf4677d5e650d7915e50
NMeekGit <noahmeeker@gmail.com>
2024-03-19 20:09:57 -0700

    found out one cannot pad and color a string at the same time

--- src/output.ts
+++ src/output.ts
@@ -14,12 +14,16 @@ const output = (geo: GeoData, wth: WthData, args: any) => {
 const printExpand = (geo: GeoData, wth: WthData) => {
 
     // print header
-    console.log(``.padEnd(w, `-`));
-    console.log(` Weather Data`.padEnd(w/2), `Time: [${getTime()}]`.padStart(w/2));
-    console.log(``.padEnd(w, `-`));
+    console.log(`\n Weather Data`.padEnd(w/2), `Time: [${getTime()}]`.padStart(w/2));
+    console.log(` `.padEnd(w, `-`));
 
-    console.log(`> Location`.padEnd(w/2), `${geo.city}, ${geo.state_prov.slice(0, 2).toUpperCase()}`.padStart(w/2));
-    console.log(`> Current Temp`.padEnd(w/2), `${wth.current.temp_f} ^F`.padStart(w/2));
+    console.log(`> City`.padEnd(w/2), `${geo.city}`.padStart(w/2));
+    console.log(`> State/Province`.padEnd(w/2), `${geo.state_prov}`.padStart(w/2));
+    console.log(`> Condition`.padEnd(w/2), `${wth.current.condition.text}`.padStart(w/2));
+    console.log(`> Current Temp`.padEnd(w/2), `${wth.current.temp_f}^F`.padStart(w/2));
+    console.log(`> Humidity`.padEnd(w/2), `${wth.current.humidity}%`.padStart(w/2));
+    console.log(`> Feels Like`.padEnd(w/2), `${wth.current.feelslike_f}^F`.padStart(w/2));
+    console.log(``);
 }
 
 export default output;
