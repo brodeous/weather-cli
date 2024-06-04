f551587f499a8964b92835b52d7bb0c6a024cce0
NoahM <noahmeeker@gmail.com>
2024-06-03 22:54:26 -0700

    added fetch error checking

--- src/datafetch.ts
+++ src/datafetch.ts
@@ -30,25 +30,33 @@ export type WthData = {
 
 export const fetchGeoData = async () => {
     
+    try{
         const res = await fetch(`https://api.ipgeolocation.io/ipgeo?apiKey=${config.ip_geo_api}&fields=geo`);
         const data = await res.json() as GeoData;
 
         return data;
+    } catch (e) {
+        throw new Error(`[\x1b[33mFETCH\x1b[0m] Invalid Geolocation API Key`);
+    }
 }
 
 export const fetchWthData = async (geo: GeoData) => {
     let param = "";
 
-    if (geo.zip !== "") {
-        param = geo.zip;
-    } else if (geo.latitude !== "") {
-        param = geo.latitude + ',' + geo.longitude;
-    } else {
-        param = geo.city; // be given by either user or geo api
+    try {
+        if (geo.zip !== "") {
+            param = geo.zip;
+        } else if (geo.latitude !== "") {
+            param = geo.latitude + ',' + geo.longitude;
+        } else {
+            param = geo.city; // be given by either user or geo api
+        }
+
+        const res = await fetch(`http://api.weatherapi.com/v1/current.json?key=${config.weather_api}&q=${param}`);
+        const data = await res.json() as WthData;
+
+        return data;
+    } catch (e) {
+        throw new Error(`[\x1b[33mFETCH\x1b[0m] Invalid Weather API Key`);
     }
-
-    const res = await fetch(`http://api.weatherapi.com/v1/current.json?key=${config.weather_api}&q=${param}`);
-    const data = await res.json() as WthData;
-
-    return data;
 }
